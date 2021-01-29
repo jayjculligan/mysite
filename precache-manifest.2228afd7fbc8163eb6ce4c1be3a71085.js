2f37e1e116b4965f6d9eceb3bacc5164e18a02f3
kudo sudo <kudosudo@kudos-MacBook-Pro.local>
2021-01-28 19:10:29 -0800

    Updates

---
+++ precache-manifest.2228afd7fbc8163eb6ce4c1be3a71085.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384a6996b5cec854e11c9c3278383928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b18b9d83d45d4838bafc",
+    "url": "/static/css/2.d84e6e44.chunk.css"
+  },
+  {
+    "revision": "e25ddaf72f08f35eb08b",
+    "url": "/static/css/main.379afb1d.chunk.css"
+  },
+  {
+    "revision": "b18b9d83d45d4838bafc",
+    "url": "/static/js/2.29e087f9.chunk.js"
+  },
+  {
+    "revision": "e25ddaf72f08f35eb08b",
+    "url": "/static/js/main.a60a6fae.chunk.js"
+  },
+  {
+    "revision": "78e07a4c50461a2c97e5",
+    "url": "/static/js/runtime~main.7cd80bd8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
